225e0717b177afc2a7766b4510c0ae6bdff50e57
thomas.zh <thomas.zh@qq.com>
2024-11-14 10:52:18 +0800

    feat: 树节点API

--- src/hooks/api-biztree.js
+++ src/hooks/api-biztree.js
@@ -3,19 +3,25 @@ import usePostData from './usePostData';
 import usePutData from './usePutData';
 import useDeleteData from './useDeleteData';
 
-// 查询树，以树状结构返回
+/** 
+ * 查询树，以树状结构返回
+ */
 export const fatchBiztree = (nodeid) => {
   const { data, error, loading } = useFetchData(`/api/sys/trees/${nodeid}/tree`);
   return { data, error, loading };
 };
 
-// 查询树，以数组结构返回
+/** 
+ * 查询树，以数组结构返回
+ */
 export const fatchBiztreeAsTable = (nodeid) => {
   const { data, error, loading } = useFetchData(`/api/sys/trees/${nodeid}/table`);
   return { data, error, loading };
 };
 
-// 创建根节点
+/** 
+ * 创建根节点
+ */
 export const createBiztreeRoot = (rootid, title, description) => {
   const { data, error, loading } = usePostData(`/api/sys/trees/root`, {
     rootid: rootid,
@@ -25,7 +31,9 @@ export const createBiztreeRoot = (rootid, title, description) => {
   return { data, error, loading };
 };
 
-// 创建树节点
+/** 
+ * 创建树节点
+ */
 export const createBiztreeNode = (pid, title, description) => {
   const { data, error, loading } = usePostData(`/api/sys/trees/node`, {
     pid: pid,
@@ -35,7 +43,9 @@ export const createBiztreeNode = (pid, title, description) => {
   return { data, error, loading };
 };
 
-// 修改树节点信息
+/** 
+ * 修改树节点信息
+ */
 export const modifyBiztreeNode = (nodeid, title, description) => {
   const { data, error, loading } = usePutData(`/api/sys/trees/${nodeid}`, {
     title: title,
@@ -44,7 +54,27 @@ export const modifyBiztreeNode = (nodeid, title, description) => {
   return { data, error, loading };
 };
 
-// 删除树节点
+/**
+ * 移动树节点位置
+ * @param {*} srcId 源节点ID
+ * @param {*} destId 目标节点ID
+ * @param {*} place 位置: before, after
+ * @param {*} relation 关系：child, brother
+ * @returns 
+ */
+export const moveBiztreeNode = (srcId, destId, place, relation) => {
+  const { data, error, loading } = usePutData(`/api/sys/trees/${nodeid}`, {
+    srcId: srcId,
+    destId: destId,
+    place: place, // 位置: before, after
+    relation: relation, // 关系：child, brother
+  });
+  return { data, error, loading };
+};
+
+/** 
+ * 删除树节点
+ */
 export const removeBiztreeNode = (nodeid) => {
   const { data, error, loading } = useDeleteData(`/api/sys/trees/${nodeid}`);
   return { data, error, loading };
