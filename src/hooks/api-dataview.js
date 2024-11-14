a275253dff5598a504b5df2c0f737baad694f75c
thomas.zh <thomas.zh@qq.com>
2024-11-14 11:01:24 +0800

    docs: API中添加注释

--- src/hooks/api-dataview.js
+++ src/hooks/api-dataview.js
@@ -3,6 +3,14 @@ import usePostData from './usePostData';
 import usePutData from './usePutData';
 import useDeleteData from './useDeleteData';
 
+/**
+ * 查询数据字典列表
+ * @param {*} page 当前页
+ * @param {*} size 每页记录数
+ * @param {*} orders 排序，数组[{'column':'name', 'dir':'asc'},{'column':'title', 'dir':'desc'}]
+ * @param {*} searchs 过滤条件，数组[{'column':'name', 'op':'eq', 'value':'系统字典'}]
+ * @returns {*} {currPage:当前页, pageSize:每页记录数, totalNum:总记录数, totalPage:总页数, datas:数组[]}
+ */
 export const fetchDataviewList = (page, size, orders, searchs) => {
   const { data, error, loading } = usePostData(`/api/dataview/list`, {
     page: page,
--- src/hooks/api-dictionary.js
+++ src/hooks/api-dictionary.js
@@ -3,15 +3,20 @@ import usePostData from './usePostData';
 import usePutData from './usePutData';
 import useDeleteData from './useDeleteData';
 
-/** 
+/**
  * 查询数据字典列表
+ * @param {*} page 当前页
+ * @param {*} size 每页记录数
+ * @param {*} orders 排序，数组[{'column':'name', 'dir':'asc'},{'column':'title', 'dir':'desc'}]
+ * @param {*} searchs 过滤条件，数组[{'column':'name', 'op':'eq', 'value':'系统字典'}]
+ * @returns {*} {currPage:当前页, pageSize:每页记录数, totalNum:总记录数, totalPage:总页数, datas:数组[]}
  */
 export const fetchDisctionaryList = (page, size, orders, searchs) => {
   const { data, error, loading } = usePostData(`/api/sys/dictionaries/list`, {
     page: page,
     size: size,
-    orders: orders, // [{'column':'name', 'dir':'asc'},{'column':'title', 'dir':'desc'}]
-    searchs: searchs, // [{'column':'name', 'op':'eq', 'value':'系统字典'}]
+    orders: orders,
+    searchs: searchs,
   });
   return { data, error, loading };
 };
