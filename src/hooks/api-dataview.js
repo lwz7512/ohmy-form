c173b628ee07f8dc53c4a8aef6abc1049a188942
thomas.zh <thomas.zh@qq.com>
2024-11-14 11:06:10 +0800

    docs: API中添加注释

--- src/hooks/api-dataview.js
+++ src/hooks/api-dataview.js
@@ -9,7 +9,7 @@ import useDeleteData from './useDeleteData';
  * @param {*} size 每页记录数
  * @param {*} orders 排序，数组[{'column':'name', 'dir':'asc'},{'column':'title', 'dir':'desc'}]
  * @param {*} searchs 过滤条件，数组[{'column':'name', 'op':'eq', 'value':'系统字典'}]
- * @returns {*} {currPage:当前页, pageSize:每页记录数, totalNum:总记录数, totalPage:总页数, datas:数组[]}
+ * @returns {*} {currPage:当前页, pageSize:每页记录数, totalNum:总记录数, totalPage:总页数, datas:数组[{每条记录为字典},{}]}
  */
 export const fetchDataviewList = (page, size, orders, searchs) => {
   const { data, error, loading } = usePostData(`/api/dataview/list`, {
