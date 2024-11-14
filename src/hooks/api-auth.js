df27ce3a12953c2a74c8d81601b3f0fd5fa6333d
thomas.zh <thomas.zh@qq.com>
2024-11-14 13:20:22 +0800

    feat: 增加修改密码API

--- src/hooks/api-auth.js
+++ src/hooks/api-auth.js
@@ -3,8 +3,11 @@ import usePostData from './usePostData';
 import usePutData from './usePutData';
 import useDeleteData from './useDeleteData';
 
-/** 
+/**
  * 登录
+ * @param {*} username 
+ * @param {*} password 请使用md5(pwd)加密后传入此参数
+ * @returns 
  */
 export const login = (username, password) => {
   const { data, error, loading } = usePostData(`/api/auth/v5/login`, {
@@ -20,5 +23,19 @@ export const login = (username, password) => {
  */
 export const logout = () => {
   const { data, error, loading } = useDeleteData(`/api/auth/v5/logout`);
+  return { data, error, loading };
+};
+
+/**
+ * 修改密码
+ * @param {*} oldPwd 请使用md5(pwd)加密后传入此参数
+ * @param {*} newPwd 请使用md5(pwd)加密后传入此参数
+ * @returns 
+ */
+export const changePassword = (oldPwd, newPwd) => {
+  const { data, error, loading } = usePostData(`/api/auth/v5/change-pwd`, {
+    oldPwd: oldPwd,
+    newPwd: newPwd,
+  });
   return { data, error, loading };
 };
